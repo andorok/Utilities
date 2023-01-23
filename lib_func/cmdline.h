730ff27aa3c0f2599f68c4ec6f122d7dfad7d051
andorok <andorok@gmail.com>
2023-01-23 09:00:12 +0300

    добавил получение значения типа double из сомандной строки

--- lib_func/cmdline.h
+++ lib_func/cmdline.h
@@ -64,4 +64,54 @@ int get_cmdl_arg(int &ii, char *argv[], const char *name, int type, int def_val)
 	return val;
 }
 
+double get_cmdl_arg(int &ii, char *argv[], const char *name, int type, double def_val)
+{
+	double val = def_val;
+	char	*pLin, *endptr;
+
+	switch (type)
+	{
+	case 0:
+	{	// без числа
+		if (!strcmp(argv[ii], name))
+		{
+			val = 1;
+			printf("Command line: %s\n", name);
+		}
+		break;
+	}
+	case 1:
+	{	// буква с числом
+		if (tolower(argv[ii][1]) == name[1])
+		{
+			pLin = &argv[ii][2];
+			if (argv[ii][2] == '\0')
+			{ // число задано через пробел
+				ii++;
+				pLin = argv[ii];
+			}
+			val = strtod(pLin, &endptr);
+			printf("Command line: %s%.2f\n", name, val);
+		}
+		break;
+	}
+	case 2:
+		if (strstr(argv[ii], name))
+		{
+			int len = (int)strlen(name);
+			pLin = &argv[ii][len];
+			if (argv[ii][len] == '\0')
+			{
+				ii++;
+				pLin = argv[ii];
+			}
+			val = strtod(pLin, &endptr);
+			printf("Command line: %s%.2f\n", name, val);
+		}
+		break;
+	}
+
+	return val;
+}
+
 #endif	// _CMDLINE_ARG_H_
